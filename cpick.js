40f2675ad62bb7bbf9336747441a8834b4f7e392
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-10-30 01:03:13 -0300

    Increase timeout duration from 1s to 2s

--- cpick.js
+++ cpick.js
@@ -50,7 +50,7 @@ const cpick = async () => {
           return null;
         }
       });
-      await new Promise((r) => setTimeout(r, 1000));
+      await new Promise((r) => setTimeout(r, 2000));
     }
 
     await new Promise((r) => setTimeout(r, 5000));
